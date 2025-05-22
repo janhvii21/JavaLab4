46ddc5954f76950918675b5241a7278bc0dddf6c
janhvii21 <janavipratihast00@gmail.com>
2025-05-22 08:33:46 +0545

    first commit

---
+++ Lab4/src/Interfacedemo/Messaging.java
@@ -0,0 +1,13 @@
+package Interfacedemo;
+
+public interface Messaging 
+{
+    public String getMessage();
+    public void print();
+    public String getRecipient();
+
+    default void printConnection() 
+    {
+        System.out.println("Connection is successful");
+    }
+}
---
+++ Lab4/src/Interfacedemo/MessagingService.java
@@ -0,0 +1,18 @@
+package Interfacedemo;
+
+public class MessagingService 
+{
+   public static void main(String[] args) 
+   {
+        Messaging sms = new SMS();
+        sms.printConnection();
+        sms.print();
+
+        System.out.println(); // Blank line
+
+        Messaging email = new Email();
+        email.printConnection();
+        email.print();
+    }
+
+}
